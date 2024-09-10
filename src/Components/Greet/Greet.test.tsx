d9f887493c087d9c1748cee357f1cafcc44fc5e3
Amulya AT <Amulya.a.t007@gmail.com>
2024-09-10 11:36:36 +0530

    Nested

--- src/Components/Greet/Greet.test.tsx
+++ src/Components/Greet/Greet.test.tsx
@@ -1,18 +1,18 @@
-import React from "react"
-import { render ,screen} from "@testing-library/react"
-import { Greet } from "./Greet"
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { Greet } from "./Greet";
 
-describe ('Greet',()=>{
-    test('Greet render second time',()=>{
-            render(<Greet/>)
-            const textElement=screen.getByText('Hello')
-            expect (textElement).toBeInTheDocument()
-        })
-        test('Greet renders with name',()=>{
-            render(<Greet name="amulya"/>)
-            const textElement=screen.getByText('Hello amulya')
-            expect(textElement).toBeInTheDocument()
-        }) 
-})
-
-
+describe.only("Greet", () => {
+  test("Greet render second time", () => {
+    render(<Greet />);
+    const textElement = screen.getByText("Hello");
+    expect(textElement).toBeInTheDocument();
+  });
+});
+describe("Nested", () => {
+  test("Greet render with name", () => {
+    render(<Greet name="amulya" />);
+    const textElement = screen.getByText("Hello amulya");
+    expect(textElement).toBeInTheDocument();
+  });
+});
