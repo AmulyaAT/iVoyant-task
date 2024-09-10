56696ba4e0f7c2ce6378d43f8e59c9194e321273
Amulya AT <Amulya.a.t007@gmail.com>
2024-09-10 11:15:15 +0530

    grouping

--- src/Components/Greet/Greet.test.tsx
+++ src/Components/Greet/Greet.test.tsx
@@ -2,14 +2,17 @@ import React from "react"
 import { render ,screen} from "@testing-library/react"
 import { Greet } from "./Greet"
 
-test('Greet render second time',()=>{
-    render(<Greet/>)
-    const textElement=screen.getByText('Hello')
-    expect (textElement).toBeInTheDocument()
+describe ('Greet',()=>{
+    test('Greet render second time',()=>{
+            render(<Greet/>)
+            const textElement=screen.getByText('Hello')
+            expect (textElement).toBeInTheDocument()
+        })
+        test('Greet renders with name',()=>{
+            render(<Greet name="amulya"/>)
+            const textElement=screen.getByText('Hello amulya')
+            expect(textElement).toBeInTheDocument()
+        }) 
 })
-test('Greet renders with name',()=>{
-    render(<Greet name="amulya"/>)
-    const textElement=screen.getByText('Hello amulya')
-    expect(textElement).toBeInTheDocument()
-})
+
 
