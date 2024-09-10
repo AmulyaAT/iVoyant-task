7ace7c10792913ca529172c57ebf6e72aacadc0e
Amulya AT <Amulya.a.t007@gmail.com>
2024-09-10 11:14:36 +0530

    changed

--- src/Components/Greet/Greet.test.tsx
+++ src/Components/Greet/Greet.test.tsx
@@ -1,13 +1,15 @@
 import React from "react"
 import { render ,screen} from "@testing-library/react"
 import { Greet } from "./Greet"
+
 test('Greet render second time',()=>{
     render(<Greet/>)
     const textElement=screen.getByText('Hello')
     expect (textElement).toBeInTheDocument()
 })
-test('Greet render with name',()=>{
+test('Greet renders with name',()=>{
     render(<Greet name="amulya"/>)
     const textElement=screen.getByText('Hello amulya')
     expect(textElement).toBeInTheDocument()
 })
+
