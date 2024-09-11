418f64826fc42e2d52fb3a30e57646c4e9628a0e
Amulya AT <Amulya.a.t007@gmail.com>
2024-09-11 09:29:20 +0530

    user login details using usestate

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { LoggedIn } from './Components/State/LoggedIn'
+import { User } from './Components/State/User'
 
 function App() {
   
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <LoggedIn/>
+      <User/>
     </>
   )
 }
---
+++ src/Components/State/User.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+
+type AuthUser={
+    name:string
+    email:string
+}
+
+export const User=()=>{
+    const [user,setUser] = useState<null|AuthUser>(null)
+
+    const handleLogin =()=>{
+        setUser({
+            name:"amulya",
+            email:"abc@gmail.com"
+        })
+    }
+    const handleLogout =()=>{setUser(null)}
+    return(
+        <>
+        <button onClick={handleLogin}>Login</button>
+        <button onClick={handleLogout}>Logout</button>
+        <div>User name is {user?.name}</div>
+       <div>User email is {user?.email}</div>
+        </>
+    )
+}
